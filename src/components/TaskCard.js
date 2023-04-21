cfdd6a61962e39260937d1aa7da373517fe369e9
Vikas007Vikas <vikaskamineni7@gmail.com>
2023-04-21 14:34:30 -0700

    Updated CSS for Responsiveness

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -53,32 +53,9 @@ const TaskCard = ({taskObj, index, id, deleteTask, updateListArray}) => {
     return (
         <div className = "card-wrapper mr-5">
             <div className = "card-top" style={{"backgroundColor": colors[index%5].primaryColor}}></div>
-            {/* <div className="task-holder">
-                <div className="card-header" style={{"backgroundColor": colors[index%5].secondaryColor, "borderRadius": "10px"}}>
-                    <span>{taskObj.name}</span>
-                </div>
-                <div className="description mt-3" style={{ overflowY: showDescription ? 'auto' : 'hidden'}}>
-                    {taskObj.description}
-                </div>
-                {taskObj.description.length > 100 && (
-                    <div className="mt-2 text-right">
-                    <button className="btn btn-link p-0" onClick={() => setShowDescription(!showDescription)}>
-                        {showDescription ? 'See Less' : 'See More'}
-                    </button>
-                    </div>
-                )}
-                <div>Due on {taskObj.dueDate}</div>
-                <div className="mt-auto">
-                    <button className="btn btn-secondary mb-2" style={{ backgroundColor: colors[index%5].primaryColor }}>{taskObj.status}</button>
-                    <div style={{"position": "absolute", "right": "20px", "bottom": "20px"}}>
-                        <i className="far fa-edit mr-3 edit-icon" style={{"color": colors[index%5].primaryColor, "cursor": "pointer", "marginRight": "10px"}} onClick={() => setModal(true)}></i>
-                        <i className="fas fa-trash-alt delete-icon" style={{"color": colors[index%5].primaryColor, "cursor": "pointer"}} onClick={handleDelete}></i>
-                    </div>
-                </div>
-            </div> */}
-            <Card sx={{ width: 370, height: 230}} style={{backgroundColor: colors[index%5].secondaryColor, borderRadius: "10px"}}>
-                <CardActionArea sx={{ width: 370, height: 230}}>
-                    <CardContent sx={{ width: 370, height: 230}} style={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+            <Card sx={{height: `${100}%`}} style={{backgroundColor: colors[index%5].secondaryColor, borderRadius: "10px"}}>
+                <CardActionArea sx={{height: `${100}%`}} >
+                    <CardContent sx={{height: `${100}%`}} style={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
                         <div class="d-flex justify-content-between">
                             <Typography gutterBottom variant="h5" component="div">
                                 {taskObj.name} 
@@ -86,7 +63,7 @@ const TaskCard = ({taskObj, index, id, deleteTask, updateListArray}) => {
                             <span className="priority-symbol">{prioritySymbols[taskObj.priority]}</span>
                         </div>
                         <Typography variant="body2" color="text.secondary">
-                                <span>Description:</span>
+                                <span style={{fontWeight: "bold"}}>Task Description: </span>
                                 {taskObj.description.length > 35 ? (
                                     <div class="description" style={{ maxHeight: '80px', overflowY: 'auto', textAlign: 'start' }}>{taskObj.description}</div>
                                 ) : (
@@ -98,7 +75,7 @@ const TaskCard = ({taskObj, index, id, deleteTask, updateListArray}) => {
                                 <button className="btn btn-secondary" style={{ backgroundColor: colors[index%5].primaryColor, border: '0px' }}>{taskObj.status}</button>
                             </Typography>
                             <Typography variant="body2" color="text.secondary">
-                                <span>Due: </span><button className="btn btn-secondary" style={{ backgroundColor: colors[index%5].primaryColor, border: '0px' }}>{taskObj.dueDate}</button>
+                                <button className="btn btn-secondary" style={{ backgroundColor: colors[index%5].primaryColor, border: '0px' }}>Due on: {taskObj.dueDate}</button>
                             </Typography>
                             <div>
                                 <i className="far fa-edit mr-3 edit-icon" style={{"color" : colors[index%5].primaryColor, "cursor" : "pointer", "marginRight": "10px"}} onClick={() => setModal(true)}></i>
